7293a3ab1521366d28fb16e890b9ac6ffc96e1e8
vikkoo98 <vikkoo98@gmail.com>
2020-02-14 16:16:48 +0100

    első kommit üres projekt

--- app/src/main/java/com/kateproject/kateapp/MainActivity.kt
+++ app/src/main/java/com/kateproject/kateapp/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)

28e4c3e7a50a4bc40341dca0273e0248ff48ed96
vikkoo98 <vikkoo98@gmail.com>
2020-02-14 15:38:05 +0100

    első kommit üres projekt

---
+++ app/src/main/java/com/kateproject/kateapp/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.kateproject.kateapp.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}

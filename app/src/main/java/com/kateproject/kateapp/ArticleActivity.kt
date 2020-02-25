6e0db312b2e7ef6a09fa7a7b5ce20d2f4f225d21
vikkoo98 <vikkoo98@gmail.com>
2020-02-25 12:34:48 +0100

    hatodik kommit cikkek betöltve, kép is van

--- app/src/main/java/com/kateproject/kateapp/ArticleActivity.kt
+++ app/src/main/java/com/kateproject/kateapp/ArticleActivity.kt
@@ -1,10 +1,15 @@
 package com.kateproject.kateapp
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
 import kotlinx.android.synthetic.main.activity_article.*
+import org.sufficientlysecure.htmltextview.HtmlTextView
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import org.sufficientlysecure.htmltextview.HtmlHttpImageGetter
+
 
 class ArticleActivity : AppCompatActivity() {
 
@@ -21,13 +26,18 @@ class ArticleActivity : AppCompatActivity() {
         //contentText.text=content
         dateText.text=date
 
+
+
         if (content !=  null)
-        {
+        {/*
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 contentText.text = Html.fromHtml(content, Html.FROM_HTML_MODE_COMPACT)
             } else {
                 contentText.text = Html.fromHtml(content)
-            }
+            }*/
+
+            val htmlTextView = findViewById<HtmlTextView>(R.id.contentText)
+            htmlTextView.setHtml(content, HtmlHttpImageGetter(htmlTextView))
         }
     }
 }
